3198615dedb21733087977d1eca47f43fad8fb5f
James Li <james.li@opower.com>
2018-01-09 23:09:50 -0800

    Finishing touches on script.js, updating data format

--- script.js
+++ script.js
@@ -4,45 +4,59 @@ var isPaused = false;
 var intervalID = 0;
 var storedCount = 0;
 
-function start(counts) {
-	isCounting = true;
-	if (this.isPaused == true) {
-		this.intervalID = setInterval(() => {
-			storedCount--;
-			document.getElementById("clock").innerHTML = this.countsToClock(storedCount);
-		}, 1000);
-	}
-	else {
-		intervalID = setInterval(() => {
-			counts--;
-			this.storedCount = counts;
-			document.getElementById("clock").innerHTML = this.countsToClock(counts);
-		}, 1000);
+function start(time) {
+	var counts = clockToCounts(time);
+	console.log(counts);
+	if (isCounting == false) {
+		if (isPaused == true) {
+			this.intervalID = setInterval(() => {
+				storedCount--;
+				document.getElementById("clock").innerHTML = this.countsToClock(storedCount);
+			}, 1000);
+			isCounting = true;
+		}
+		else {
+			intervalID = setInterval(() => {
+				counts--;
+				storedCount = counts;
+				document.getElementById("clock").innerHTML = this.countsToClock(counts);
+			}, 1000);
+			isCounting = true;
+		}
 	}
 }
 
 function stop() {
-	this.isPaused = false;
-	this.isCounting = false;
+	isPaused = false;
+	isCounting = false;
 	clearInterval(intervalID);
 }
 
 function pause() {
-	this.isPaused = true;
-	this.isCounting = false;
+	isPaused = true;
+	isCounting = false;
 	clearInterval(intervalID);
 }
 
-function reset(set) {
+function reset(time) {
 	if (isCounting == false) {
-		document.getElementById("clock").innerHTML = this.countsToClock(set);
-		this.isPaused = false;
+		document.getElementById("clock").innerHTML = time;
+		isPaused = false;
 	}
 }
 
+// A function to convert counts into time format
 function countsToClock(counts) {
 	var mins = Math.floor(counts / 60);
 	var seconds = counts % 60;
-	var clock = mins + ":" + seconds;
+	var clock = mins + ":" + (seconds < 10? '0':'') + seconds;
 	return clock;
+}
+
+// A function to convert time into counts
+function clockToCounts(clock) {
+	var countArr = clock.split(":");
+	// Now convert the strings to actual numbers before summing up
+	var counts = (+countArr[0] * 60) + (+countArr[1]);
+	return counts;
 }
